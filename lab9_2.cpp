80a7540c5b7d239470a6f7ff2fe88fba3033bdde
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-28 15:50:45 +0700

    updated

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+
+int main(){
+	
+	ifstream source;
+	source.open("D:\\c++\\lab9-2562-2-phubet16132\\cheerbook.txt");
+	
+	string textline;
+	
+	ofstream dest("D:\\c++\\lab9-2562-2-phubet16132\\cheerbook_copy.txt");
+	
+	dest<<"-------------------- SOTUS ---------------------\n";
+	while(getline(source,textline)){
+		
+		dest<<textline<<endl;
+	}
+	dest<<"-------------------- SOTUS ---------------------";
+	source.close();
+	dest.close();
+	return 0;
+}
+
+
+
+
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
